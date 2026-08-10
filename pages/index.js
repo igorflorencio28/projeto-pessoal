5534e54806affff6d7d8eb970890cac8e55e668d
Igor <igor_florencio@yahoo.com>
2026-08-10 20:13:26 -0300

    Primeiro commit - Sem arquivos pesados

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return <h1>Site do melhor programador: Igor Florêncio 😎</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
+
+export default Home;
